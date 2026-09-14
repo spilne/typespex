0fdc015b4509b7ce928daafb8d7b3ea740aa6e85
zakolenko <zakolenkoroman@gmail.com>
2026-09-14 06:52:46 +0300

    Cover direct transport execution from the runtime sources

--- packages/http-server/test/transport-dispatch.test.ts
+++ packages/http-server/test/transport-dispatch.test.ts
@@ -10,7 +10,7 @@ import {
   type HttpRouterOptions,
   type MatchedRequestContext,
   type ServerOperation,
-} from "@typespex/http-server";
+} from "../src/index.js";
 
 function operation(): ServerOperation<void, string> {
   return {
